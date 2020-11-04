6bd3643b33a28ef9c17f21218eb4c8f230b7ed77
Cameron Aziz <caziz@theknotww.com>
2020-11-04 08:48:31 -0800

    startup

--- src/utils/startup.ts
+++ src/utils/startup.ts
@@ -4,7 +4,7 @@ const startup = (outputChannel: vscode.OutputChannel) => {
 	outputChannel.appendLine('The Knot Worldwide Assistant Extension is now active.');
 	outputChannel.appendLine('  This extension is not for public distribution.');
 	outputChannel.appendLine('  Many thanks for the team that helped put this together.');
-	outputChannel.appendLine('  Report all bugs on Slack at `#tkww-vscode-extension` or email tkww-vscode-extension@theknotww.com.');
+	outputChannel.appendLine('  Report all bugs on Slack at `#tkww-vscode-extension`.');
 	outputChannel.appendLine('');
 };
 
