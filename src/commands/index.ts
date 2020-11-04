a09d7aa0d85984204ec12c5f775935d380618bd9
Cameron Aziz <caziz@theknotww.com>
2020-11-04 08:46:14 -0800

    filename change

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,3 @@
-export { default as convertFile } from './convertFiles';
+export { default as convertFile } from './convertFile';
 export { default as convertWorkspace } from './convertWorkspace';
 export { default as toggleConvertFileOnSave } from './toggleConvertFileOnSave';
